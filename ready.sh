1c72042dc84943447e8daca027c34feaea96ca52
sidpixel <45841651+sidpixel@users.noreply.github.com>
2023-09-22 00:34:58 +0800

    up ready.sh

--- ready.sh
+++ ready.sh
@@ -9,7 +9,7 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 拉取微信推送插件
 git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
