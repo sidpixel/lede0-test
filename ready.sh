d4b2f12518994ff05c05ebfc2e965e751d4f2d59
sidpixel <45841651+sidpixel@users.noreply.github.com>
2023-09-21 17:59:24 +0800

    up fees.conf

--- ready.sh
+++ ready.sh
@@ -16,6 +16,7 @@ git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 # 拉取oaf流控插件
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 #拉取passwall,openclash
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git openclash https://github.com/vernesong/OpenClash.git;master" >> "feeds.conf.default"
 
