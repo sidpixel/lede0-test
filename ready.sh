ef16bea3af330f941f035aa7bab838d31f1607c8
sidpixel <45841651+sidpixel@users.noreply.github.com>
2023-09-22 11:18:41 +0800

    up test

--- ready.sh
+++ ready.sh
@@ -16,9 +16,9 @@ git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 # 拉取oaf流控插件
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 #拉取passwall,openclash
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
-echo "src-git openclash https://github.com/vernesong/OpenClash.git;master" >> feeds.conf.default
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" |tee -a feeds.conf.default
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main"  |tee -a  feeds.conf.default
+echo "src-git openclash https://github.com/vernesong/OpenClash.git;master"  |tee -a  feeds.conf.default
 
 
 #定义时间变量
